3a2bb88138ff5f18c372e2cd2acc2f487307e90a
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-07-10 18:50:07 +0200

    Finish creating app theme Defined colors for light and dark theme Defined spacing Changed status bar and system bar colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,15 +50,19 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    // Core
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
+    // Jetpack Compose
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    // Testing and tooling
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -66,4 +70,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // Status bar color
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
 }
--- app/src/main/java/com/example/geomate/ui/theme/Color.kt
+++ app/src/main/java/com/example/geomate/ui/theme/Color.kt
@@ -1,11 +1,101 @@
 package com.example.geomate.ui.theme
 
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object GeoMateColors {
+    // Specific colors for light theme
+    val Brown50 = Color(0xFF7B5401)
+    val Beige30 = Color(0xFFF4E8DA)
+    val Beige10 = Color(0xFFFFF7F0)
+    val Gray50 = Color(0xFF464845)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    // Specific colors for dark theme
+    val Orange40 = Color(0xFFFFAC62)
+    val Gray60 = Color(0xFF444444)
+    val Gray80 = Color(0xFF363636)
+    val Gray90 = Color(0xFF2A2A2A)
+
+    // Common colors
+    val White = Color(0xFFFFFFFF)
+    val Red40 = Color(0xFFFF4040)
+}
+
+interface GoeMateColorScheme{
+    val primary: Color
+    val onPrimary: Color
+    val secondary: Color
+    val onSecondary: Color
+    val surface: Color
+    val surfaceVariant: Color
+    val onSurface: Color
+    val onSurfaceVariant: Color
+    val backgroundLight: Color
+    val backgroundDark: Color
+    val onBackground: Color
+    val error: Color
+}
+
+object GeoMateLightColorScheme : GoeMateColorScheme {
+    override val primary: Color = GeoMateColors.Brown50
+    override val onPrimary: Color = GeoMateColors.White
+    override val secondary: Color = GeoMateColors.Beige30
+    override val onSecondary: Color = GeoMateColors.Gray50
+    override val surface: Color = GeoMateColors.Beige10
+    override val surfaceVariant: Color = GeoMateColors.White
+    override val onSurface: Color = GeoMateColors.Gray50
+    override val onSurfaceVariant: Color = GeoMateColors.Gray50
+    override val backgroundLight: Color = GeoMateColors.Beige10
+    override val backgroundDark: Color = GeoMateColors.Beige30
+    override val onBackground: Color = GeoMateColors.Gray50
+    override val error: Color = GeoMateColors.Red40
+}
+
+object GeoMateDarkColorScheme : GoeMateColorScheme {
+    override val primary: Color = GeoMateColors.Orange40
+    override val onPrimary: Color = GeoMateColors.Gray90
+    override val secondary: Color = GeoMateColors.Gray80
+    override val onSecondary: Color = GeoMateColors.White
+    override val surface: Color = GeoMateColors.Gray80
+    override val surfaceVariant: Color = GeoMateColors.Gray60
+    override val onSurface: Color = GeoMateColors.White
+    override val onSurfaceVariant: Color = GeoMateColors.White
+    override val backgroundLight: Color = GeoMateColors.Gray90
+    override val backgroundDark: Color = GeoMateColors.Gray90
+    override val onBackground: Color = GeoMateColors.White
+    override val error: Color = GeoMateColors.Red40
+}
+
+fun mapColors(
+    darkTheme: Boolean,
+    colorScheme: GoeMateColorScheme
+) = if (darkTheme) {
+    darkColorScheme(
+        primary = colorScheme.primary,
+        onPrimary = colorScheme.onPrimary,
+        secondary = colorScheme.secondary,
+        onSecondary = colorScheme.onSecondary,
+        surface = colorScheme.surface,
+        onSurface = colorScheme.onSurface,
+        surfaceVariant = colorScheme.surfaceVariant,
+        onSurfaceVariant = colorScheme.onSurfaceVariant,
+        background = colorScheme.backgroundLight,
+        onBackground = colorScheme.onBackground,
+        error = colorScheme.error
+    )
+} else {
+    lightColorScheme(
+        primary = colorScheme.primary,
+        onPrimary = colorScheme.onPrimary,
+        secondary = colorScheme.secondary,
+        onSecondary = colorScheme.onSecondary,
+        surface = colorScheme.surface,
+        onSurface = colorScheme.onSurface,
+        surfaceVariant = colorScheme.surfaceVariant,
+        onSurfaceVariant = colorScheme.onSurfaceVariant,
+        background = colorScheme.backgroundLight,
+        onBackground = colorScheme.onBackground,
+        error = colorScheme.error
+    )
+}
