7a123c38ee5c2c453a330d42f1eac196e07bfa08
Daniel Bortsov <wretched666bunny@gmail.com>
2023-11-02 19:40:27 +0100

    chore(notifications): Deleted NotificatonDatasource, NotificationRemoteDataSouce, NotificationsRepository
    Moved getSentListFlow to FriendShipDataSource because i did not see sense in keeping those classes. This function belongs to Friendship, because it is with FriendShip request and nothing else.

--- app/src/main/java/com/example/geomate/data/datasources/NotificationsDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.geomate.data.datasources
-
-import com.example.geomate.data.models.FriendshipRequest
-import kotlinx.coroutines.flow.Flow
-
-
-interface NotificationsDataSource {
-    suspend fun getFriendshipRequestsFlow(userId: String): Flow<List<FriendshipRequest>>
-}
