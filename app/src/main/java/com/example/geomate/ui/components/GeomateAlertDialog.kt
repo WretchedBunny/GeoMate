0948503b4d07eb72b9569b7efa0086eb8184931a
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-11-11 14:29:16 +0100

    Small changes to the dialog composable

--- app/src/main/java/com/example/geomate/ui/components/GeomateAlertDialog.kt
+++ app/src/main/java/com/example/geomate/ui/components/GeomateAlertDialog.kt
@@ -1,15 +1,15 @@
 package com.example.geomate.ui.components
 
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import com.example.geomate.R
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GeomateAlertDialog(
     onDismissRequest: () -> Unit,
@@ -41,14 +41,15 @@ fun GeomateAlertDialog(
         onDismissRequest = onDismissRequest,
         confirmButton = {
             TextButton(onClick = onConfirmation) {
-                Text("Confirm")
+                Text(text = stringResource(id = R.string.alert_dialog_confirmation_confirm))
             }
         },
         dismissButton = {
             TextButton(onClick = onDismissRequest) {
-                Text("Dismiss")
+                Text(text = stringResource(id = R.string.alert_dialog_confirmation_dismiss))
             }
         },
-        containerColor = MaterialTheme.colorScheme.background
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = modifier
     )
 }
