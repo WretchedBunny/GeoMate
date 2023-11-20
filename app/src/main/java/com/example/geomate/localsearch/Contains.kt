b79bffc410793d3271ce1390763aa8823eb1155b
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-11-20 09:37:10 +0100

    Simplified Contains rule

--- app/src/main/java/com/example/geomate/localsearch/Contains.kt
+++ app/src/main/java/com/example/geomate/localsearch/Contains.kt
@@ -2,6 +2,6 @@ package com.example.geomate.localsearch
 
 object Contains : Rule {
     override fun match(value: String, searchQuery: String): Boolean {
-        return value.lowercase().contains(searchQuery.lowercase())
+        return value.contains(searchQuery, true)
     }
 }
